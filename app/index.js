f8c0738b0fed97fd12cc21ba080ac5c92342aaf3
Gabriela Santos <gabi.gsantos@hotmail.com>
2017-11-07 16:49:09 -0300

    Menu e Rodapé

--- app/index.js
+++ app/index.js
@@ -3,7 +3,27 @@ const app = express()
 const path = require("path")
 
 app.set("view engine","pug")
-app.set("views", path.join(__dirname))
+app.set("views", path.join(__dirname, "views"))
+
+app.get("/", (req,res)=>{
+    res.render("index")
+})
+
+app.get("/index.html", (req,res)=>{
+    res.render("index")
+})
+
+app.get("/elements.html", (req,res)=>{
+    res.render("elements")
+})
+
+app.get("/generic.html", (req,res)=>{
+    res.render("generic")
+})
+
+app.get("/contact.html", (req,res)=>{
+    res.render("contact")
+})
 
 app.get("*", (req,res)=>{
     res.sendFile(path.join(__dirname, "../public/"+req.params[0]))
